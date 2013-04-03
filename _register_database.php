758fe810a58616da9517913ec3778ab831e54b9c
Damian Mooyman <damian.mooyman@gmail.com>
2013-04-03 17:19:26 +1300

    API Upgraded module to use new database ORM

---
+++ _register_database.php
@@ -0,0 +1,42 @@
+<?php
+
+// PDO connector for MS SQL Server
+DatabaseAdapterRegistry::register(array(
+	'class' => 'MSSQLPDODatabase',
+	'title' => 'SQL Server 2008 (using PDO)',
+	'helperPath' => dirname(__FILE__).'/code/MSSQLDatabaseConfigurationHelper.php',
+	'supported' => (class_exists('PDO') && in_array('sqlsrv', PDO::getAvailableDrivers())),
+	'missingExtensionText' =>
+		'Either the <a href="http://www.php.net/manual/en/book.pdo.php">PDO Extension</a> or 
+		the <a href="http://www.php.net/manual/en/ref.pdo-sqlsrv.php">SQL Server PDO Driver</a> 
+		are unavailable. Please install or enable these and refresh this page.'
+));
+
+// Basic driver using sqlsrv connector
+DatabaseAdapterRegistry::register(array(
+	'class' => 'MSSQLDatabase',
+	'title' => 'SQL Server 2008 (using sqlsrv)',
+	'helperPath' => dirname(__FILE__).'/code/MSSQLDatabaseConfigurationHelper.php',
+	'supported' => function_exists('sqlsrv_connect'),
+	'missingExtensionText' =>
+		'The <a href="http://www.microsoft.com/sqlserver/2005/en/us/PHP-Driver.aspx">sqlsrv</a>
+		PHP extensions is not available. Please install or enable it and refresh this page.',
+	'fields' => array_merge(DatabaseAdapterRegistry::get_default_fields(), array(
+		// @todo - do we care about windows authentication for PDO/SQL Server?
+		'windowsauthentication' => array(
+			'title' => 'Use Windows authentication? (leave blank for false)',
+			'default' => ''
+		)
+	))
+));
+
+// MS Azure uses an online database
+DatabaseAdapterRegistry::register(array(
+	'class' => 'MSSQLAzureDatabase',
+	'title' => 'MS Azure Database (using sqlsrv)',
+	'helperPath' => dirname(__FILE__).'/code/MSSQLDatabaseConfigurationHelper.php',
+	'supported' => function_exists('sqlsrv_connect'),
+	'missingExtensionText' =>
+		'The <a href="http://www.microsoft.com/sqlserver/2005/en/us/PHP-Driver.aspx">sqlsrv</a>
+		PHP extension is not available. Please install or enable it and refresh this page.'
+));
---
+++ code/SQLServerQuery.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * A result-set from a MSSQL database.
+ * 
+ * @package mssql
+ */
+class SQLServerQuery extends SS_Query {
+
+	/**
+	 * The SQLServerConnector object that created this result set.
+	 * 
+	 * @var SQLServerConnector
+	 */
+	private $connector;
+
+	/**
+	 * The internal MSSQL handle that points to the result set.
+	 * 
+	 * @var resource
+	 */
+	private $handle;
+
+	/**
+	 * Hook the result-set given into a Query class, suitable for use by sapphire.
+	 * @param SQLServerConnector $connector The database object that created this query.
+	 * @param resource $handle the internal mssql handle that is points to the resultset.
+	 */
+	public function __construct(SQLServerConnector $connector, $handle) {
+		$this->connector = $connector;
+		$this->handle = $handle;
+	}
+
+	public function __destruct() {
+		if(is_resource($this->handle)) {
+			sqlsrv_free_stmt($this->handle);
+		}
+	}
+
+	public function seek($row) {
+		if(!is_resource($this->handle)) return false;
+
+		user_error('MSSQLQuery::seek() not supported in sqlsrv', E_USER_WARNING);
+	}
+
+	public function numRecords() {
+		if(!is_resource($this->handle)) return false;
+
+		// WARNING: This will only work if the cursor type is scrollable!
+		if(function_exists('sqlsrv_num_rows')) {
+			return sqlsrv_num_rows($this->handle);
+		} else {
+			user_error('MSSQLQuery::numRecords() not supported in this version of sqlsrv', E_USER_WARNING);
+		}
+	}
+
+	public function nextRecord() {
+		if(!is_resource($this->handle)) return false;
+
+		if($data = sqlsrv_fetch_array($this->handle, SQLSRV_FETCH_ASSOC)) {
+			// special case for sqlsrv - date values are DateTime coming out of the sqlsrv drivers,
+			// so we convert to the usual Y-m-d H:i:s value!
+			foreach($data as $name => $value) {
+				if($value instanceof DateTime) $data[$name] = $value->format('Y-m-d H:i:s');
+			}
+			return $data;
+		} else {
+			// Free the handle if there are no more results - sqlsrv crashes if there are too many handles
+			sqlsrv_free_stmt($this->handle);
+			$this->handle = null;
+		}
+
+		return false;
+	}
+
+}
