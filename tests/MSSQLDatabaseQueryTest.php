2a580942bcd65644d115a151ff3e61c5e0022705
Loz Calver <lozcalver@bigfork.co.uk>
2018-05-25 09:16:01 +0100

    Fix AppVeyor, update unit tests

--- tests/MSSQLDatabaseQueryTest.php
+++ tests/MSSQLDatabaseQueryTest.php
@@ -6,28 +6,28 @@ use SilverStripe\Dev\TestOnly;
 
 class MSSQLDatabaseQueryTest extends SapphireTest
 {
+    protected static $fixture_file = 'MSSQLDatabaseQueryTest.yml';
 
-    public static $fixture_file = 'MSSQLDatabaseQueryTest.yml';
-
-    protected $extraDataObjects = array(
-        'MSSQLDatabaseQueryTestDataObject'
-    );
+    public static function getExtraDataObjects()
+    {
+        return ['MSSQLDatabaseQueryTestDataObject'];
+    }
 
     public function testDateValueFormatting()
     {
         $obj = $this->objFromFixture('MSSQLDatabaseQueryTestDataObject', 'test-data-1');
-        $this->assertEquals('2012-01-01', $obj->obj('TestDate')->Format('Y-m-d'), 'Date field value is formatted correctly (Y-m-d)');
+        $this->assertEquals('2012-01-01', $obj->obj('TestDate')->Format('y-MM-dd'), 'Date field value is formatted correctly (y-MM-dd)');
     }
 
     public function testDatetimeValueFormatting()
     {
         $obj = $this->objFromFixture('MSSQLDatabaseQueryTestDataObject', 'test-data-1');
-        $this->assertEquals('2012-01-01 10:30:00', $obj->obj('TestDatetime')->Format('Y-m-d H:i:s'), 'Datetime field value is formatted correctly (Y-m-d H:i:s)');
+        $this->assertEquals('2012-01-01 10:30:00', $obj->obj('TestDatetime')->Format('y-MM-dd HH:mm:ss'), 'Datetime field value is formatted correctly (y-MM-dd HH:mm:ss)');
     }
 }
+
 class MSSQLDatabaseQueryTestDataObject extends DataObject implements TestOnly
 {
-
     private static $db = array(
         'TestDate' => 'Date',
         'TestDatetime' => 'Datetime'
